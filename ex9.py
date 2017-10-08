3d350b30023927eb3a12395329d51e5d2533d9eb
AbyAbyss <abyrocksaji@gmail.com>
2017-10-08 16:48:15 +0530

    ex9.py typed in python3

---
+++ ex9.py
@@ -0,0 +1,12 @@
+# ex9.py typed in python3
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: %s" %days)     #with %s and %days
+print ("Here are the months: ", months)   #without %s separated by ','
+
+print ("""There's something going on here. 
+With the three double- quotes. 
+We'll be able to type as much as we liske. 
+Even 4 lines if we want, or 5, or 6.""")
