80cd7f6ead2f5e25a4386ad54ea329fb1bf4c793
AbyAbyss <abyrocksaji@gmail.com>
2017-11-16 09:29:37 -0500

    chapter3_lists

--- python_crash_course/bicycles.py
+++ python_crash_course/bicycles.py
@@ -1,3 +1,16 @@
 # Starting with lists
 bicycles = ['trek', 'cannondale', 'redline', 'specilalized']
 print(bicycles)
+
+# printing first element
+print(bicycles[0])
+
+# using title
+print(bicycles[0].title())
+
+# to return/print the last item in the list
+print(bicycles[-1])
+
+# using individual values from the list
+messaage = 'My first bicycles was ' + bicycles[0].title() + "."
+print(messaage)
---
+++ python_crash_course/chapter3/try_it_yourself.py
@@ -0,0 +1,29 @@
+# 3.1 Names:
+
+names = ['aby', 'abyss', 'sanju', 'kiddo']
+
+# printing each name separatly
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+# 3.2 greeting
+
+message = 'Hello, ' + names[0] + '.'
+print(message)
+message = 'Hello, ' + names[1] + '.'
+print(message)
+message = 'Hello, ' + names[2] + '.'
+print(message)
+message = 'Hello, ' + names[3] + '.'
+print(message)
+
+
+# 3.3 your own list
+
+my_likes = ['car', 'bike', 'plane']
+
+for i in my_likes:
+    print('I would like to own a ' + i + '.')
