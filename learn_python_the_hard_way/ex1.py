a4dcfa084383d0b619f647170ead61ca69f428b2
AbyAbyss <abyrocksaji@gmail.com>
2017-11-22 16:46:35 +0530

    ex11-12-13_ book

--- learn_python_the_hard_way/ex1.py
+++ learn_python_the_hard_way/ex1.py
@@ -1,3 +1,4 @@
+# ex1.py done in python3
 print("Hello World!")
 print("Hi there")
 # pritn "Commented"
---
+++ learn_python_the_hard_way/ex11_ex12.py
@@ -0,0 +1,7 @@
+# ex11.py and ex12.py done in python3
+# raw_inupt() was renamed to input() in python3
+# input() will always take string, int(input()) is used to convert age to int
+age = int(input("How old are you? "))
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
